e8955fac6ee9459a252ef4bf635d3878f65e02f1
Ola Yeku <ooyeku@gmail.com>
2023-02-05 22:27:44 -0600

    added documentations to meditation.go

--- wellness/meditate.go
+++ wellness/meditate.go
@@ -1,18 +1,29 @@
 package main
 
-// breathing exercises
+// Command line meditation/breathing exercise tool for when youre in the flow and dont want to leave the terminal, but
+// need to take a break... By default the runs 5 rounds of 5 second inhales, a 5 second hold, and a 5 second exhale.
+// Arguments:
+// 	-n = number of rounds
+// 	-inhale = number of seconds for inhale cycle
+// 	-exhale = number of seconds for exhale cycle
+// 	-hold = number of seconds for breathe holding cycle
+// Example:
+//		go run meditation -n 8 -inhale 5 -exhale 3 -hold 4
+// - runs an 8 round meditation cycle with a 5 second inhale, 4 second hold, and 3 second exhale
 import (
 	"flag"
 	"fmt"
 	"time"
 )
 
-func getReady() { // getting ready for breathing exercise
+// getReady creates buffer to prepare for inhale/exhale cycle
+func getReady() {
 	fmt.Println("Relax and Prepare to inhale...")
 	time.Sleep(time.Second * 5)
 
 }
 
+// breatheIn uses an inhale, exhale, and hold variable to create the main meditation loop
 func breatheIn(inhale, exhale, hold int) {
 	// buffer to get ready
 	time.Sleep(time.Second * 3)
@@ -34,6 +45,7 @@ func breatheIn(inhale, exhale, hold int) {
 	fmt.Println("Well Done!!")
 }
 
+// TODO: delete
 func waiting() {
 	for {
 		time.Sleep(time.Second)
